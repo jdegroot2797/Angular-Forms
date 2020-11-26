5043bdd7e23ef26d6a992178e0ff222014c73e31
Jonathan De Groot <jdegroot2797@gmail.com>
2020-11-26 14:35:04 -0500

    initial commit

--- src/app/starterTemplateForm/starterTemplateForm.component.ts
+++ src/app/starterTemplateForm/starterTemplateForm.component.ts
@@ -12,12 +12,38 @@ export class StarterTemplateFormComponent implements OnInit {
 
     ngOnInit() { 
         this.customer = {
-            firstName: 'Fred Template'
+            firstName:  'John',
+            lastName:   'Doe',
+            address:    '123 Main St',
+            city:       'Calgary',
+            province:   'Alberta',
+            country:    'Canada',
+            postalCode: 'A1A 1A1',
+            phoneNumber:'111-222-3333',
+            email:      'jdoe@email.com'
         };
     }
 
     onSubmit() {
-        this.message = 'You typed: ' + this.customer.firstName;
+        this.message =
+        
+        `<div class="userInfo">
+            <h2>
+                You typed
+            </h2>
+        <b>First Name:</b>     ${this.customer.firstName}<br> 
+        <b>Last Name:</b>      ${this.customer.lastName}<br> 
+        <b>Address:</b>        ${this.customer.address}<br> 
+        <b>City:</b>           ${this.customer.city}<br> 
+        <b>Province:</b>       ${this.customer.province}<br> 
+        <b>Country:</b>        ${this.customer.country}<br> 
+        <b>Postal Code:</b>    ${this.customer.postalCode}<br> 
+        <b>Phone Number:</b>   ${this.customer.phoneNumber}<br> 
+        `;
+
+         document.body.innerHTML = this.message;
     }
+
+
 
 }
